e790436a7e5a56cd16cdbb61013ab00d893016e4
jperalta2202 <jperalta@uc.cl>
2022-05-04 17:58:17 -0400

    more fixes

--- B2CClient.js
+++ B2CClient.js
@@ -146,8 +146,11 @@ export class B2CClient {
 
   async getAccountForPolicy(policyUrl) {
     const accounts = await this.pca.getAccounts();
-    return accounts.find(account =>
-      account.identifier.includes(policyUrl.toLowerCase()),
+    return accounts.find(
+      //account => account.identifier.includes(policyUrl.toLowerCase()),
+      account.identifier.includes(
+        'https://mankukb2c.b2clogin.com/tfp/mankukb2c.onmicrosoft.com/B2C_1_susi',
+      ),
     );
   }
 }
